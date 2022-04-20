dd6361ffbfb1b18bfe9ff7d56c7a0e76f269966a
MohitMehto98 <mohitmahto98@gmail.com>
2022-04-21 03:31:31 +0530

    feat: update code from mohit

--- api/book.js
+++ api/book.js
@@ -8,166 +8,168 @@ const Review = require("../models/review");
 // ====================== public api ===========================
 
 exports.getBookList = async (req, res) => {
-  Book.find()
-    .then((response) => {
-      res.status(200).json({ message: response });
-    })
-    .catch((error) => {
-      res.status(500);
-    });
+	Book.find()
+		.then((response) => {
+			res.status(200).json({ message: response });
+		})
+		.catch((error) => {
+			res.status(500);
+		});
 };
 
 exports.getBookById = async (req, res) => {
-  console.log(req.body);
-  Book.findOne({ _id: req.body.bookId })
-    .then((response) => {
-      res.status(200).json({ message: response });
-    })
-    .catch((error) => {
-      res.status(500);
-    });
+	console.log(req.body);
+	Book.findOne({ _id: req.body.bookId })
+		.then((response) => {
+			res.status(200).json({ message: response });
+		})
+		.catch((error) => {
+			res.status(500);
+		});
 };
 exports.getBookReview = async (req, res) => {
-  Review.find({ bookId: req.body.bookId })
-    .then((response) => {
-      res.status(200).json({ message: response });
-    })
-    .catch((error) => {
-      res.status(500);
-    });
+	Review.find({ bookId: req.body.bookId })
+		.then((response) => {
+			res.status(200).json({ message: response });
+		})
+		.catch((error) => {
+			res.status(500);
+		});
 };
 // =============================================================
 
 // ====================== private api ==========================
 
 exports.getBookByUser = async (req, res) => {
-  Book.find({ userId: req.body.userId })
-    .then((response) => {
-      res.status(200).json({ message: response });
-    })
-    .catch((error) => {
-      res.status(500);
-    });
+	Book.find({ userId: req.body.userId })
+		.then((response) => {
+			res.status(200).json({ message: response });
+		})
+		.catch((error) => {
+			res.status(500);
+		});
 };
 
 exports.getBookAutherByUser = async (req, res) => {
-  BookAuther.findOne({ bookId: req.body.bookId })
-    .then((response) => {
-      res.status(200).json({ message: response });
-    })
-    .catch((error) => {
-      res.status(500);
-    });
+	BookAuther.findOne({ bookId: req.body.bookId })
+		.then((response) => {
+			res.status(200).json({ message: response });
+		})
+		.catch((error) => {
+			res.status(500);
+		});
 };
 
 // ==============================================================
 
 exports.createBook = async (req, res) => {
-  Book.create({
-    userId: req.body.userId,
-    image: req.body.image,
-    name: req.body.name,
-    price: req.body.price,
-    about: req.body.about,
-    detail: req.body.detail,
-  })
-    .then((response) => {
-      res.status(200).json({ message: response });
-    })
-    .catch((error) => {
-      res.status(500);
-    });
+	Book.create({
+		userId: req.body.userId,
+		image: req.body.image,
+		name: req.body.name,
+		price: req.body.price,
+		about: req.body.about,
+		detail: req.body.detail,
+		university: req.body.university,
+	})
+		.then((response) => {
+			res.status(200).json({ message: response });
+		})
+		.catch((error) => {
+			res.status(500);
+		});
 };
 
 exports.createAuther = async (req, res) => {
-  BookAuther.create({
-    bookId: req.body.bookId,
-    AutherImage: req.body.authImage,
-    AutherName: req.body.authName,
-    AutherDetail: req.body.authDetail,
-  })
-    .then((response) => {
-      res.status(200).json({ message: response });
-    })
-    .catch((error) => {
-      res.status(500);
-    });
+	BookAuther.create({
+		bookId: req.body.bookId,
+		AutherImage: req.body.authImage,
+		AutherName: req.body.authName,
+		AutherDetail: req.body.authDetail,
+	})
+		.then((response) => {
+			res.status(200).json({ message: response });
+		})
+		.catch((error) => {
+			res.status(500);
+		});
 };
 
 exports.createReview = async (req, res) => {
-  Review.create({
-    bookId: req.body.bookId,
-    userName: req.body.userName,
-    review: req.body.review,
-  })
-    .then((response) => {
-      res.status(200).json({ message: response });
-    })
-    .catch((error) => {
-      res.status(500);
-    });
+	Review.create({
+		bookId: req.body.bookId,
+		userName: req.body.userName,
+		review: req.body.review,
+	})
+		.then((response) => {
+			res.status(200).json({ message: response });
+		})
+		.catch((error) => {
+			res.status(500);
+		});
 };
 
 // ================================================================
 
 exports.editBook = async (req, res) => {
-  let update = {
-    userId: req.body.userId,
-    image: req.body.image,
-    name: req.body.name,
-    price: req.body.price,
-    about: req.body.about,
-    detail: req.body.detail,
-  };
-  let filter = {
-    _id: req.body.bookId,
-  };
-  Book.findOneAndUpdate(filter, update)
-    .then((response) => {
-      res.status(200).json({ message: "edit successfully !" });
-    })
-    .catch((error) => {
-      res.status(500);
-    });
+	let update = {
+		userId: req.body.userId,
+		image: req.body.image,
+		name: req.body.name,
+		price: req.body.price,
+		about: req.body.about,
+		detail: req.body.detail,
+		university: req.body.university,
+	};
+	let filter = {
+		_id: req.body.bookId,
+	};
+	Book.findOneAndUpdate(filter, update)
+		.then((response) => {
+			res.status(200).json({ message: "edit successfully !" });
+		})
+		.catch((error) => {
+			res.status(500);
+		});
 };
 
 exports.editAuther = async (req, res) => {
-  let update = {
-    bookId: req.body.bookId,
-    AutherImage: req.body.authImage,
-    AutherName: req.body.authName,
-    AutherDetail: req.body.authDetail,
-  };
-  let filter = {
-    _id: req.body.autherId,
-  };
-  BookAuther.findOneAndUpdate(filter, update)
-    .then((response) => {
-      res.status(200).json({ message: "edit successfully !" });
-    })
-    .catch((error) => {
-      res.status(500);
-    });
+	let update = {
+		bookId: req.body.bookId,
+		AutherImage: req.body.authImage,
+		AutherName: req.body.authName,
+		AutherDetail: req.body.authDetail,
+	};
+	let filter = {
+		_id: req.body.autherId,
+	};
+	BookAuther.findOneAndUpdate(filter, update)
+		.then((response) => {
+			res.status(200).json({ message: "edit successfully !" });
+		})
+		.catch((error) => {
+			res.status(500);
+		});
 };
 
 // ================================================================
 
 exports.deleteBook = async (req, res) => {
-  Book.deleteOne({ _id: req.body.bookId })
-    .then((response) => {
-      res.status(200).json({ message: "Book Deleted !" });
-    })
-    .catch((error) => {
-      res.status(500);
-    });
+	Book.deleteOne({ _id: req.body.bookId })
+		.then((response) => {
+			res.status(200).json({ message: "Book Deleted !" });
+		})
+		.catch((error) => {
+			res.status(500);
+		});
 };
 
 exports.getBookByUniversity = async (req, res) => {
-  Book.find({ university: req.body.university })
-    .then((response) => {
-      res.status(200).json({ message: response });
-    })
-    .catch((error) => {
-      res.status(500);
-    });
+	Book.find({ university: req.body.university })
+		.then((response) => {
+			res.status(200).json({ message: response });
+		})
+		.catch((error) => {
+			res.status(500);
+		});
 };
